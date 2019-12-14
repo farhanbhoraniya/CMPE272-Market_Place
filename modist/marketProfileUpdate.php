6f367e2bcd60c43a4dad30e02c2b921d04767f71
Karan Patel <karanpatel31105115@gmail.com>
2019-12-14 13:20:01 -0800

    update UI

--- modist/marketProfileUpdate.php
+++ modist/marketProfileUpdate.php
@@ -7,56 +7,194 @@
         echo "Not Logged In";
     }
 ?>
-<html>
-    <head>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    </head>
-
-    <body>
-        <br>
-        <div class = "container">
-            <table style = 'text-align:center' class ='table table-condensed'>
-                <form action = "/marketProfileUpdater.php" method = "POST" id="updateForm" >   
-                    <tr>
-                        <td colspan = "2">
-                            <strong>Update Your Profile</strong>
-                        </td>
-                    </tr>        
-                    <tr>
-                        <td>
-                            <label for="name">Name</label>
-                        </td>
-
-                        <td>
-                            <input type="text" class="form-control" id="name" placeholder="Enter Name" name="name" value = "<?php echo $_SESSION['name']; ?>" required>
-                        </td>
-                    </tr>      
-                    
-                    <tr>
-                        <td>
-                            <label for="password">Password</label>
-                        </td>
-
-                        <td>
-                            <input type="password" class="form-control" id="password" placeholder="Enter Password" name="password" required>
-                        </td>
-                    </tr>
-                                   
-                    <tr>
-                        <td  colspan = "2">
-                            <input type="submit" value = "Update Profile" class="btn btn-default"/>
-                        </td>
-                    </tr>
-                </form>
-            </table>
-        </div>
-        <br>
-    </body>
-
-
-</html>
 
 <?php
-    unset($_SESSION["userAdded"]);
-    unset($_SESSION["error"]);
+session_start();
+?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Sign In Form</title>
+
+        <!-- Font Icon -->
+        <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
+
+        <!-- Main css -->
+        <link rel="stylesheet" href="css/style_1.css">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700" rel="stylesheet">
+
+        <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+        <link rel="stylesheet" href="css/animate.css">
+
+        <link rel="stylesheet" href="css/owl.carousel.min.css">
+        <link rel="stylesheet" href="css/owl.theme.default.min.css">
+        <link rel="stylesheet" href="css/magnific-popup.css">
+
+        <link rel="stylesheet" href="css/aos.css">
+
+        <link rel="stylesheet" href="css/ionicons.min.css">
+
+        <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+        <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+
+        <link rel="stylesheet" href="css/flaticon.css">
+        <link rel="stylesheet" href="css/icomoon.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="oi oi-menu"></span> Menu
+            </button>
+
+            <div class="collapse navbar-collapse" id="ftco-nav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active"><a href="index.php" class="nav-link">Home</a></li>
+                    <li class="nav-item active"><a href="shop.php" class="nav-link">Products</a></li>
+                    <li class="nav-item active"><a href="login.php" class="nav-link">Log In</a></li>
+                    <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="main" style="padding: 150px;">
+
+        <!-- Sing in  Form -->
+        <section class="sign-in">
+            <div class="container">
+                <div class="signin-content">
+                    <div class="signin-image">
+                        <figure><img src="images/update.jpg" alt="sing up image"></figure>
+                        <a href="signup.php" class="signup-image-link"></a>
+                    </div>
+
+                    <div class="signin-form">
+                        <h2 class="form-title">Update</h2>
+                        <form class="register-form"  action = "marketUserLogin.php" method = "POST" onsubmit="login()" id="loginForm" >
+                            <div class="form-group">
+                                <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                                <input type="text" name= "name" id="name" placeholder="Enter Name" value = "<?php echo $_SESSION['name']; ?>" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="your_pass"><i class="zmdi zmdi-lock"></i></label>
+                                <input type="password" name="passwordLogin" id="passwordLogin" placeholder="Password"/>
+                            </div>
+                            <div class = "form-group">
+                                <?php
+                                if(isset($_SESSION["error"])){
+                                    $error = $_SESSION["error"];
+                                    echo "<span>$error</span>";
+                                }
+                                ?>
+                            </div>
+                            <div class="form-group form-button">
+                                <input type="submit" name="signin" id="signin" class="form-submit" value="Update Profile"/>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </div>
+    <footer class="ftco-footer bg-light ftco-section" style="padding-top: 0px;">
+        <div class="container">
+            <div class="row mb-5">
+                <div class="col-md">
+                    <div class="ftco-footer-widget mb-4">
+                        <h2 class="ftco-heading-2">MARXET PLACE</h2>
+                        <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
+                            <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                            <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                            <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="ftco-footer-widget mb-4 ml-md-5">
+                        <h2 class="ftco-heading-2">Menu</h2>
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="py-2 d-block">Shop</a></li>
+                            <li><a href="#" class="py-2 d-block">About</a></li>
+                            <li><a href="#" class="py-2 d-block">Journal</a></li>
+                            <li><a href="#" class="py-2 d-block">Contact Us</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="ftco-footer-widget mb-4">
+                        <h2 class="ftco-heading-2">Help</h2>
+                        <div class="d-flex">
+                            <ul class="list-unstyled mr-l-5 pr-l-3 mr-4">
+                                <li><a href="#" class="py-2 d-block">Shipping Information</a></li>
+                                <li><a href="#" class="py-2 d-block">Returns &amp; Exchange</a></li>
+                                <li><a href="#" class="py-2 d-block">Terms &amp; Conditions</a></li>
+                                <li><a href="#" class="py-2 d-block">Privacy Policy</a></li>
+                            </ul>
+                            <ul class="list-unstyled">
+                                <li><a href="#" class="py-2 d-block">FAQs</a></li>
+                                <li><a href="#" class="py-2 d-block">Contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="ftco-footer-widget mb-4">
+                        <h2 class="ftco-heading-2">Have a Questions?</h2>
+                        <div class="block-23 mb-3">
+                            <ul>
+                                <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
+                                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
+                                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 text-center">
+
+                    <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- JS -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="js/main_1.js"></script>
+    <script>
+        function login(){
+            let username = document.getElementById('emailLogin').value;
+            let password = document.getElementById('passwordLogin').value;
+
+            if(username.length > 0 && password.length > 0){
+                document.getElementById("loginForm").submit();
+            }else{
+                alert("Invalid Credentials");
+            }
+        }
+    </script>
+    </body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+    </html>
+
+
+
+<?php
+unset($_SESSION["userAdded"]);
+unset($_SESSION["error"]);
 ?>
