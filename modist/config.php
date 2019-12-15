b2b88cfd643830c2004ba5a2d4b2335025cd45f5
Farhan <farhanbhoraniya@gmail.com>
2019-12-14 18:43:09 -0800

    Minor change

--- modist/config.php
+++ modist/config.php
@@ -2,7 +2,7 @@
 
 define('CLIENT_ID', '715534942487-r12ok0jf1e25g7v0eavprbmnofqu99gp.apps.googleusercontent.com');
 define('CLIENT_SECRET', 'hs1tIZ1MXScgO0cpQ7wN1jl8');
-define('CLIENT_REDIRECT_URL', 'http://localhost/cmpe272-market_place/modist/google_login.php');
+define('CLIENT_REDIRECT_URL', 'http://www.marketplace.manavrajvanshi.com/google_login.php');
 // session_start();
 
 ?>
